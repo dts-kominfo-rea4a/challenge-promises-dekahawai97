b0fe010a04a4e5edba1986a31a4936716b6929ed
Komputer-IT <hisbulhawai16@gmail.com>
2022-09-20 11:26:35 +0700

    feat: Tugas 2 Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,27 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+let bioskopMarah = 0;
+let bioskopTidakMarah = 0;
+const promiseOutput = emosi => {
+  if (emosi === 'marah') {
+    return promiseTheaterIXX().then((value) => {
+      value.forEach((array) => {
+        if (array.hasil === 'marah') {
+            bioskopMarah++;
+        } else if (array.hasil === 'tidak marah') {
+          bioskopTidakMarah++;
+        } else {
+          'error';
+        }
+      });
+      return bioskopMarah
+    });
+  }
+}
+
+promiseOutput('tidak marah')
+promiseOutput('marah')
 
 module.exports = {
   promiseOutput,
